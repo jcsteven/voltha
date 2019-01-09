64b849964676d3766d3d2bcd0d94d181203cc5a3
jcsteven <jcsteven.yu@gmail.com>
2019-01-09 17:14:24 +0800

    Add build script

---
+++ 1.0_Build_VOLTHA.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# -------------------------------------------------------
+# Automatic preparation script for rtk_openwrt
+# JC Yu,     Novenber 26,2015
+# -------------------------------------------------------
+# IMPORTANT:
+#   When use: './<this script file>  '
+# -------------------------------------------------------
+TODAY=`date +"%Y-%m%d-%H%M"`
+BLOG_DIR="Build-log"
+BLOG_DIR_WK=${HOME}/${BLOG_DIR}
+Record_File=${BLOG_DIR_WK}/voltha-1.6.0-fixed-log.txt
+
+
+[ -d $BLOG_DIR_WK ] || mkdir $BLOG_DIR_WK
+s_time=$(date +%s)
+echo "==============================================================================" |  tee -a $Record_File
+echo "Start:ASFVOLT16-Build-VOLTHA:${TODAY}" | tee -a $Record_File
+
+
+
+#cd ${VOLTHA_DIR}
+. env.sh
+REPOSITORY=voltha/  make fetch
+REPOSITORY=voltha/  make install-protoc
+REPOSITORY=voltha/  make build
+#cd  ${PPWW}
+
+e_time=$(date +%s)
+elap_s=$((e_time-s_time))
+ss=$((elap_s%60))
+mm=$(((elap_s/60)%60))
+hh=$((elap_s/3600))
+echo "------------------------------------------------------------------------------" | tee -a $Record_File
+echo "End  :ASFVOLT16-Build-VOLTHA:${TODAY}" | tee -a $Record_File
+echo "Build total time: $hh:$mm:$ss" | tee -a $Record_File
+echo "==============================================================================" | tee -a $Record_File
+
+
+
+
+
+
+
